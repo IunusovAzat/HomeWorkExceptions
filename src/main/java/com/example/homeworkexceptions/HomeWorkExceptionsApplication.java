aee64704ff88f334b6a8b32c1f7a5fa9fa3a7e40
Юнусов Азат <kill-azat@mail.ru>
2023-01-26 14:36:06 +0300

    Первый коммит

---
+++ src/main/java/com/example/homeworkexceptions/HomeWorkExceptionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeworkexceptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkExceptionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWorkExceptionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homeworkexceptions/HomeWorkExceptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homeworkexceptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkExceptionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
